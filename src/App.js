db257f8ebe474f08b9381fae302f619a99eadc06
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-07 11:18:37 +0530

    updatting to redirecct to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import React from "react";
 import HomePage from "./pages/homepage/Homepage.component";
@@ -42,7 +42,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />{" "}
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)} />
   
         </Switch>
       </div>
@@ -51,12 +51,18 @@ class App extends React.Component {
  
  
 }
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
+
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
 
