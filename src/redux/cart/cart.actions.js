5132b3a12ba32c322b3d2a9e7e7a8777e71ed81d
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-12 20:05:19 +0530

    cartItem get updated

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,7 @@ export const toggleCartHidden = () => ({
     type: CartActionType.TOGGLE_CART_HIDDEN
 })
 
+export const addItem = (item) => ({
+    type: CartActionType.ADD_ITEM,
+    payload: item
+})
