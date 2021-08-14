dbb12ffb4f1ffd417645d464416cd71f27a6a477
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-14 20:32:09 +0530

    send

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-
+import {store, persistor} from './redux/store';
+import {PersistGate} from 'redux-persist/es/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
 <App />
+</PersistGate>
 </BrowserRouter>
   </Provider>,
 
