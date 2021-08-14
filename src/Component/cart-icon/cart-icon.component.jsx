37283e2ea47d5b4f022d357432c06eaca75fe6e2
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-14 11:57:02 +0530

    updating application

--- src/Component/cart-icon/cart-icon.component.jsx
+++ src/Component/cart-icon/cart-icon.component.jsx
@@ -3,17 +3,20 @@ import {connect} from "react-redux";
 import {toggleCartHidden} from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoopingIcon } from '../../assets/11.2 shopping-bag.svg'
 import './cart-icon.styles.scss'
+import { selectCartItemCount } from "../../redux/cart/cart.selector";
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden,itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoopingIcon className="shooping-con"/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
     </div>
 )
 
 const mapDispatchtoProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 })
+const mapStatetoProps = state => ({
+    itemCount : selectCartItemCount(state)
+})
 
-
-export default connect(null, mapDispatchtoProps)(CartIcon);
+export default connect(mapStatetoProps, mapDispatchtoProps)(CartIcon);
