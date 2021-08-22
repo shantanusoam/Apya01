bcbf6fa8e85d9831e0aeb9d12e2f253dce11a2fd
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-22 20:32:43 +0530

    send

--- src/Component/Sectionabout.js
+++ src/Component/Sectionabout.js
@@ -24,9 +24,9 @@ const sectionAbout = () => {
                     </div>
                     <div class="col-1-of-2">
                         <div class="composition">
-                            <img src="img/uwp478489.jpeg" alt="Photo 1" class="composition__photo composition__photo--p1"/>
-                            <img src="img/uwp478478.jpeg" alt="Photo 2" class="composition__photo composition__photo--p2"/>
-                            <img src="img/football.png" alt="Photo 3" class="composition__photo composition__photo--p3"/>
+                            <img src="./img/uwp478489.jpeg" alt="Photo 1" class="composition__photo composition__photo--p1"/>
+                            <img src="./img/uwp478478.jpeg" alt="Photo 2" class="composition__photo composition__photo--p2"/>
+                            <img src="./img/football.png" alt="Photo 3" class="composition__photo composition__photo--p3"/>
                         </div>
                     </div>
                 </div>
--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -3,10 +3,10 @@ import Directory from '../../Component/directory/directory.componrnt';
 import Footer from '../../Component/Footer';
 import Header from '../../Component/Header';
 import Main from '../../Component/Main';
-
+import Navbar from '../../Component/Navbar';
 import Navigation from '../../Component/Navigation';
 import Popup from '../../Component/Popup';
-
+import './homepage.style.scss'
 const HomePage = () => (
     <div className="homepage">
         
