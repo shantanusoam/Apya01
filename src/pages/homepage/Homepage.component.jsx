462c4a3dd78dbf5e7322aff60232d8d8d148678f
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-22 19:15:00 +0530

    send

--- src/pages/homepage/Homepage.component.jsx
+++ src/pages/homepage/Homepage.component.jsx
@@ -3,7 +3,7 @@ import Directory from '../../Component/directory/directory.componrnt';
 import Footer from '../../Component/Footer';
 import Header from '../../Component/Header';
 import Main from '../../Component/Main';
-import Navbar from '../../Component/Navbar';
+
 import Navigation from '../../Component/Navigation';
 import Popup from '../../Component/Popup';
 import './homepage.style.scss'
