ab30c1634a4726a1c78ec378261dddc205aae810
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-08-30 21:10:04 +0530

    sed

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ class App extends React.Component {
         
         
         <Switch>
-          <Route exact path="/apya01" component={HomePage} />
+          <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route path="/checkout" component={CheckoutPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)} />
