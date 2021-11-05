d787ef1ad92c76520d0ca7aca589c5718f35e962
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2021-11-05 12:09:04 +0530

    send

--- src/Component/footer.js
+++ src/Component/footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
                 <div class="col-1-of-2">
                     <div class="footer__navigation">
                         <ul class="footer__list">
-                            <li class="footer__item"><a  href="/#" class="footer__link">Company Sector 4, 4/141, Madhav Puram, Meerut-250002, Uttar Pradesh, India </a></li>
+                            <li class="footer__item"><a  href="/#" class="footer__link">Company </a></li>
                             <li class="footer__item"><a  href="https://www.indiamart.com/apyaindustry/enquiry.html" class="footer__link">Contact us</a></li>
                             <li class="footer__item"><a  href="/#" class="footer__link">Carrers</a></li>
-                            <li class="footer__item"><a  href="/#" class="footer_ olicy</a></li>
+                            <li class="footer__item"><a  href="/#" class="footer__link">Privacy policy</a></li>
                             <li class="footer__item"><a  href="/#" class="footer__link">Terms</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-1-of-2">
-                    <p class="footer__copyright">
+                     <p class="footer__copyright">
                         Built by <a  href="/#" class="footer__link">Jonas Schmedtmann</a> for his online course <a  href="/#" class="footer__link">Advanced CSS and Sass</a>.
                         Copyright &copy; by Jonas Schmedtmann. You are 100% allowed to use this webpage for both personal
                         and commercial use, but NOT to claim it as your own design. A credit to the original author, Jonas
